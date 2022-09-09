6db15c359b0bb20c803fbb9c20e690168ce521df
DouglasFuelber <contato@douglasfuelber.com>
2022-09-09 14:06:14 -0300

    Display toppings in summary page only if any was added

--- src/tests/OrderPhase.test.jsx
+++ src/tests/OrderPhase.test.jsx
@@ -84,3 +84,24 @@ test("Order phases for happy path", async () => {
 	await screen.findByRole("spinbutton", { name: "Vanilla" });
 	await screen.findByRole("checkbox", { name: "Cherries" });
 });
+
+test("Toppings header is not on summary page if no toppings orderer", async () => {
+	render(<App />);
+
+	const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+	userEvent.clear(vanillaInput);
+	userEvent.type(vanillaInput, "1");
+
+	const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
+	userEvent.clear(chocolateInput);
+	userEvent.type(chocolateInput, "2");
+
+	const orderSummaryButton = screen.getByRole("button", { name: /order sundae/i });
+	userEvent.click(orderSummaryButton);
+
+	const scoopsHeading = screen.getByRole("heading", { name: "Scoops: $6.00" });
+	expect(scoopsHeading).toBeInTheDocument();
+
+	const toppingsHeading = screen.queryByRole("heading", { name: /toppings/i });
+	expect(toppingsHeading).not.toBeInTheDocument();
+});
