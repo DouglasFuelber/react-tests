7e26ce1633f71d52c691b5bf548597518437e31b
DouglasFuelber <contato@douglasfuelber.com>
2022-09-09 14:40:30 -0300

    Disable order button if no scoops ordered

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from "../../../test-utils/testing-library-uti
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
+import userEvent from "@testing-library/user-event";
 
 test("handles error from scoops and toppings routes", async () => {
 	server.resetHandlers(
@@ -16,4 +17,20 @@ test("handles error from scoops and toppings routes", async () => {
 
 		expect(alerts).toHaveLength(2);
 	});
+});
+
+test("disable order button if there are no scoops ordered", async () => {
+	render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+	let orderButton = screen.getByRole("button", { name: /order sundae/i });
+	expect(orderButton).toBeDisabled();
+
+	const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+	userEvent.clear(vanillaInput);
+	userEvent.type(vanillaInput, "1");
+	expect(orderButton).toBeEnabled();
+
+	userEvent.clear(vanillaInput);
+	userEvent.type(vanillaInput, "0");
+	expect(orderButton).toBeDisabled();
 });
